075a632a5527baf90b041295e12e66640fed02d6
jlap96 <lilyaguilar9@gmail.com>
2023-08-26 23:48:57 -0600

    Added table with styles to view payments

--- UsuarioCo/Inicio.php
+++ UsuarioCo/Inicio.php
@@ -9,7 +9,7 @@ if ($_SESSION["TipoUsuario"] !== "CO") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/estilos.css">
+    <link rel="stylesheet" href="../css/estilos.css">
     <link rel="stylesheet" href="estilosCo.css">
     <title>Inicio</title>
 </head>
@@ -20,7 +20,7 @@ if ($_SESSION["TipoUsuario"] !== "CO") {
             <div class="barra-menu">
                 <nav class="enlaces">
                     <ul>
-                        <li><a href="Inicio.html">Inicio</a></li>
+                        <li><a href="Inicio.php">Inicio</a></li>
                         <li><a href="">Consultar pagos</a></li>
                         <li><a href="../Home/Index.html">Salir</a></li>
                     </ul>
